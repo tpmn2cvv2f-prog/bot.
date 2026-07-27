1c77242efed5d4ab7d3dfe411b0fbdac50f28317
tpmn2cvv2f-prog <tpmn2cvv2f@privaterelay.appleid.com>
2026-07-28 02:55:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from telethon import TelegramClient
+
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH", "")
+
+client = TelegramClient("session_name", API_ID, API_HASH)
+
+async def main():
+    print("Bot is running!")
+
+if __name__ == "__main__":
+    with client:
+        client.loop.run_until_complete(main())
