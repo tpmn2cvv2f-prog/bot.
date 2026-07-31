403c8f3398fa601cd031e70a43f14a591b174235
tpmn2cvv2f-prog <tpmn2cvv2f@privaterelay.appleid.com>
2026-07-31 13:08:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ from telethon import TelegramClient
 
 api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
-phone = os.environ.get("TG_PHONE")
+bot_token = os.environ.get("BOT_TOKEN")
 
-client = TelegramClient("session_name", api_id, api_hash)
-
+client = TelegramClient("bot_session", api_id, api_hash)
 
 async def main():
-  await client.start(phone=phone)
-  print("Bot is running successfully!")
-
+    await client.start(bot_token=bot_token)
+    print("Bot is running successfully!")
 
 with client:
-  client.loop.run_until_complete(main())
+    client.loop.run_until_complete(main())
