54aeec7514e5785bf1bdeaf6053dd25c9f61369c
tpmn2cvv2f-prog <tpmn2cvv2f@privaterelay.appleid.com>
2026-07-31 06:14:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ api_id = int(os.environ.get("API_ID"))
 api_hash = os.environ.get("API_HASH")
 phone = os.environ.get("TG_PHONE")
 
-# تمرير الجوال للـ client يمنعه من طلب input()
 client = TelegramClient("session_name", api_id, api_hash)
 
 
